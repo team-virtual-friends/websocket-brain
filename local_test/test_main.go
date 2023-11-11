8851d394ddd99c7d14858a13a377e67710438b42
Yufan Lu <myhappyland3113@gmail.com>
2023-11-11 15:56:07 -0800

    replace langauge detector

--- local_test/test_main.go
+++ local_test/test_main.go
@@ -2,12 +2,27 @@ package main
 
 import (
 	"fmt"
+	"time"
 
-	"github.com/rylans/getlang"
+	"github.com/pemistahl/lingua-go"
 )
 
 func main() {
-	info := getlang.FromString("Wszyscy ludzie rodzą się wolni i równi w swojej godności i prawach")
-	fmt.Println(info.LanguageCode(), info.LanguageName(), info.Confidence())
-	fmt.Printf("%+v\n", info)
+	// info := getlang.FromString("hi, I am number 1.")
+	// fmt.Println(info.LanguageCode(), info.LanguageName(), info.Confidence())
+	// fmt.Printf("%+v\n", info)
+
+	text := "hi, I am number 1."
+
+	detector := lingua.NewLanguageDetectorBuilder().FromAllLanguages().
+		Build()
+
+	now := time.Now()
+	// for i := 0; i < 10000; i++ {
+	if language, exists := detector.DetectLanguageOf(text); exists {
+		fmt.Println(language.IsoCode639_1().String())
+		fmt.Println(language.IsoCode639_3().String())
+	}
+	// }
+	fmt.Println(time.Now().Sub(now))
 }
